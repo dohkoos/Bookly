1711f82d3755362ff148c8d6a2c333899905a6e3
Peter Wong <dohkoos@gmail.com>
2014-12-21 14:18:32 +0800

    Upgraded API level from 9-17 to 15-21

--- src/com/codemany/bookly/MainActivity.java
+++ src/com/codemany/bookly/MainActivity.java
@@ -15,7 +15,7 @@ public class MainActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
+        setContentView(R.layout.activity_main);
 
         searchButton = (Button)findViewById(R.id.btn_search);
         searchButton.setOnClickListener(new OnClickListener() {
